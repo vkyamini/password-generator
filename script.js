39a75a0e77d81f73edb0a11a9251187c8755fcf9
Yamini VK <yamini@Ashishs-MacBook-Pro.local>
2023-03-30 18:49:58 -0700

    added the code

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-//GIVEN I need a new, secure password
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria 
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN asked for character types to include in the password
-//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
-
-
-
-//var passwordLength = 128;
-
 var upperCase =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowerCase=['a','b','c','d','e','e','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var passSymbols =["!","@","#","$","%"]
@@ -128,30 +108,10 @@ alert("hai")
   
 console.log("i am passwordArray " +passwordArray);
 console.log(pswLength);
-
-
-// for(i=0;i<pswLength;i++)
-// {
-//    yourPassword = yourPassword+passwordArray[Math.floor(Math.random()*passwordArray.length)];
-// }
 var tada=[""];
-// store the random string generated
+
 tada = [storePssLength]+[storeUcAdd]+[storeLcAdd]+[symbol];
 console.log(tada, "tada");
-
-// var password = "";
-
-// for (var i=0; i<tada.length; i++) {
-//    password = password + tada[Math.floor(Math.random() * tada.length)];
-// }
-
-// console.log(password, "password");
-
-// passwordArray.push(tada);
-// var finalPassword="";
-
-
-
 onclickBtn.addEventListener("click",function()
  {
  
